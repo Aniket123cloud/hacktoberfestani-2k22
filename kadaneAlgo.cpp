ac207541b58e90f04e523331a460e42138572d44
bhaveshgarg2000 <bhaveshgarg2005@gmail.com>
2022-10-05 09:56:15 +0530

    Leetcode 53 Maximum subArray

---
+++ kadaneAlgo.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+        int sum = 0;
+        int maxi = nums[0];
+        for (int i = 0; i < nums.size(); i++)
+        {
+            sum = sum + nums[i];
+            maxi = max(maxi, sum);
+
+            if (sum < 0)
+                sum = 0;
+        }
+        return maxi;
+    }
+};
